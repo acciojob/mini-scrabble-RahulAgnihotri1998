04b998cfca790e833532fb362c1f31bc217c10db
RahulAgnihotri1998 <rahulagnihotri1998@gmail.com>
2023-01-05 19:20:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-const textbox = document.getElementById('evaluatedText');
-const letterCount = document.getElementById('letterCount');
 
-textbox.addEventListener('keyup', function() {
-  letterCount.textContent = textbox.value.length;
-});
+    const textbox = document.getElementById('evaluatedText');
+      const letterCount = document.getElementById('letterCount');
+      
+      textbox.addEventListener('keyup', function() {
+        letterCount.textContent = textbox.value.length;
+      });
